6ab0af94971a229fc498f5e12c9f5dac6a4f8e92
root <root@vm132.sysnet.ucsd.edu>
2017-02-21 11:06:01 -0800

    Adding player

--- demo/grayscale/demo/views.py
+++ demo/grayscale/demo/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+@csrf_exempt
 def index(request):
     return render(request, 'static/index.html', context=None)
 
+@csrf_exempt
 def call_mu(request):
     if request.method == 'POST':
+	print (request)
 	url = request.POST.get('url')
 	bucket = request.POST.get('bucket')
 	print ("URL=%s,Bucket=%s") % (url, bucket)
 	response_data = {}
-	output_mpd = invoke_mu(ur, bucket)
+	output_mpd = invoke_mu(url, bucket)
 	response_data['mpd'] = output_mpd
 	return HttpResponse(
 	    json.dumps(response_data),
@@ -20,4 +30,6 @@ def call_mu(request):
 	)
 
 def invoke_mu(url, bucket):
-	return "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd"
+	mpd = "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd"
+	print (mpd)
+	return mpd
