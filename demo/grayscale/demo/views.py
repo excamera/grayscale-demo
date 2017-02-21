cc03f13eb7215668fe2af0734b21dbf7b0a86e6b
root <root@vm132.sysnet.ucsd.edu>
2017-02-21 09:04:46 -0800

    Fixing button handler

--- demo/grayscale/demo/views.py
+++ demo/grayscale/demo/views.py
@@ -5,3 +5,19 @@ from django.views.generic import TemplateView
 # Create your views here.
 def index(request):
     return render(request, 'static/index.html', context=None)
+
+def call_mu(request):
+    if request.method == 'POST':
+	url = request.POST.get('url')
+	bucket = request.POST.get('bucket')
+	print ("URL=%s,Bucket=%s") % (url, bucket)
+	response_data = {}
+	output_mpd = invoke_mu(ur, bucket)
+	response_data['mpd'] = output_mpd
+	return HttpResponse(
+	    json.dumps(response_data),
+	    content_type="application/json"
+	)
+
+def invoke_mu(url, bucket):
+	return "http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd"
