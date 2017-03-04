91864f767026ac3bbdc85dd95be643817e191847
kvasukib <kvasukib@eng.ucsd.edu>
2017-03-04 10:23:24 -0800

    Adding latest changes

--- demo/grayscale/demo/views.py
+++ demo/grayscale/demo/views.py
@@ -22,36 +22,48 @@ def index(request):
 @csrf_exempt
 def call_mu(request):
     if request.method == 'POST':
-	print (request.POST)
-	url = request.POST.get("url")
-	print ("URL=%s") % url
-	response_data = invoke_mu(url)
-	return HttpResponse(
-	    json.dumps(response_data),
-	    content_type="application/json"
-	)
+        print (request.POST)
+        url = request.POST.get("url")
+        print ("URL=%s") % url
+        response_data = invoke_mu(url)
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+
+@csrf_exempt
+def call_mu_stub(request):
+    response_data = {}
+    response_data['input_signed_url'] = 'http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd'
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
+
+def invoke_pipeline(url):
+    pass
 
 def invoke_mu(url):
-	response_data = {}
-	url = "https://s3-us-west-2.amazonaws.com/excamera-ffmpeg-input/input.mp4"
-	regex = "^https://s3-(\S+).amazonaws.com/(\S+)/(\S+)$"
-	m = re.match(regex, url)
-	if m:
-		region = m.group(1)
-		bucket = m.group(2)
-		key    = m.group(3)
-	print "region=%s bucket=%s key=%s" % (region, bucket, key)
-	signed_url = sign_url.get_signed_url(3000, bucket, key)
-	print "signed_url=%s" % signed_url
-	response_data['input_signed_url'] = signed_url
+    response_data = {}
+    url = "https://s3-us-west-2.amazonaws.com/excamera-ffmpeg-input/input.mp4"
+    regex = "^https://s3-(\S+).amazonaws.com/(\S+)/(\S+)$"
+    m = re.match(regex, url)
+    if m:
+        region = m.group(1)
+        bucket = m.group(2)
+        key    = m.group(3)
+        print "region=%s bucket=%s key=%s" % (region, bucket, key)
+        signed_url = sign_url.get_signed_url(3000, bucket, key)
+        print "signed_url=%s" % signed_url
+        response_data['input_signed_url'] = signed_url
 
-	cmds = ["/home/kvasukib/lambda/pipeline/external/mu/src/lambdaize/run_pipeline.sh", 
-		bucket, 
-		key]
-	try:
-		print "Running the job in mu : "
-		output = subprocess.check_output(cmds, stderr=subprocess.STDOUT, shell=True)
-	except subprocess.CalledProcessError as exc:
-		print ("error code", exc.returncode, exc.output)
-	print "Job over from mu. Rendering the video..."
-	return response_data
+        cmds = ["/home/kvasukib/lambda/pipeline/external/mu/src/lambdaize/run_pipeline.sh", 
+                bucket, 
+                key]
+        try:
+            print "Running the job in mu : "
+            output = subprocess.check_output(cmds, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as exc:
+            print ("error code", exc.returncode, exc.output)
+            print "Job over from mu. Rendering the video..."
+            return response_data
