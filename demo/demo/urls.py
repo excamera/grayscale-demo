aa0631176e2f5ac046e86a48b33e2711b6749a63
Lixiang Ao <aolixiang@gmail.com>
2017-08-07 11:42:49 -0700

    redesign UI, use user-supplied url and pipespec

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,11 +1,19 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^call_mu/$', views.call_mu, name='call_mu'),
-    url(r'^call_mu_stub/$', views.call_mu_stub, name='call_mu_stub'),
+    url(r'^jobs/$', views.jobs, name='jobs'),
+    url(
+        r'^favicon.ico$',
+        RedirectView.as_view(
+            url=staticfiles_storage.url('favicon.ico'),
+            permanent=False),
+        name="favicon"
+    ),    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 
@@ -13,7 +13,6 @@ import os
 import uuid
 import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/pipeline/src/pipeline')
-sys.path.append(os.path.dirname(os.path.realpath(__file__))+'/pipeline/external/mu/src/lambdaize/')
 
 from service import pipeline_pb2_grpc, pipeline_pb2
 from config import settings
@@ -21,42 +20,37 @@ import time
 import grpc
 import pdb
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
-lambdas = "200"
 
-# Create your views here.
+
 @csrf_exempt
 def index(request):
     return render(request, 'static/index.html', context=None)
 
+
 @csrf_exempt
-def call_mu(request):
+def jobs(request):
+    # pdb.set_trace()
     if request.method == 'POST' and request.is_ajax():
-        url = request.POST['url']
-	logfile = request.POST['log']
-	print ("URL=%s Logfile=%s") % (url, logfile)
+        try:
+            video_link = request.POST['video_link']
+            pipespec = request.POST['pipespec']
 
-    mpd_url, err = invoke_pipeline(url)
-    response_data = {}
-    response_data['mpd_url'] = mpd_url
+            mpd_url = invoke_pipeline(video_link, pipespec)
+            response_data = {}
+            response_data['mpd_url'] = mpd_url
+            pdb.set_trace()
+            return HttpResponse(
+                    json.dumps(response_data),
+                    content_type="application/json")
+        except Exception as e:
+            return HttpResponse(content=str(e), status=500)
+    else:
+        return HttpResponseNotAllowed(['POST'])
 
-    return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
-
-@csrf_exempt
-def call_mu_stub(request):
-    response_data = {}
-    response_data['input_signed_url'] = 'http://dash.edgesuite.net/envivio/EnvivioDash3/manifest.mpd'
-    return HttpResponse(
-        json.dumps(response_data),
-        content_type="application/json"
-    )
 
 @csrf_exempt
-def invoke_pipeline(url):
+def invoke_pipeline(url, pipespec):
     print time.time(), 'FORREST: invoking pipeline:', time.time()
     if not url.startswith('s3://'):
         # for youtube videos:
@@ -69,39 +63,12 @@ def invoke_pipeline(url):
                 video_url = u
                 break
     print time.time(), 'FORREST: get actually video url'
-    pipeline_spec = open("path/to/your/pipespec/file", "r").read()
     channel = grpc.insecure_channel('%s:%d' % (settings['daemon_addr'], settings['daemon_port']))
     stub = pipeline_pb2_grpc.PipelineStub(channel)
-    response = stub.Submit(pipeline_pb2.PipelineRequest(pipeline_spec=pipeline_spec, input_urls=[video_url], options=None))
+    response = stub.Submit(pipeline_pb2.PipelineRequest(pipeline_spec=pipespec, input_urls=[video_url], options=None))
 
-    print time.time(), 'FORREST: finish pipeline, returning to browser'
-
-    pdb.set_trace()
-    return response.mpd_url, None
-
-
-@csrf_exempt
-def invoke_mu(url, logfile):
-    response_data = {}
-    regex = "^https?://s3.amazonaws.com/(\S+)/(\S+)$"
-    m = re.match(regex, url)
-    if m:
-        region = m.group(1)
-        bucket = m.group(2)
-        key    = m.group(3)
-        print "region=%s bucket=%s key=%s" % (region, bucket, key)
-        signed_url = sign_url.get_signed_url(3000, bucket, key)
-        print "signed_url=%s" % signed_url
-        response_data['input_signed_url'] = signed_url
-
-        cmds = ["/home/kvasukib/lambda/pipeline/external/mu/src/lambdaize/run_pipeline.sh", 
-                bucket, 
-                key
-		]
-        try:
-            print "Running the job in mu : "
-            output = subprocess.check_output(cmds, stderr=subprocess.STDOUT, shell=True)
-        except subprocess.CalledProcessError as exc:
-            print ("error code", exc.returncode, exc.output)
-            print "Job over from mu. Rendering the video..."
-            return response_data
+    if response.success:
+        print time.time(), 'FORREST: finish pipeline, returning to browser'
+        return response.mpd_url
+    else:
+        raise Exception(response.error_msg)
